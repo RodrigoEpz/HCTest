76b21f3f04e8ca4baa5a226e068f492adb92120f
Rodrigo Esparza <rodrigo.esparza@bestday.com>
2019-04-10 21:16:22 -0500

    Se agrega proyecto de net core para la restApi

---
+++ HCApiTest/HCApiTest/Models/Cars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCApiTest.Models
+{
+    public class Cars
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Model length only can be of 50 characters")]
+        public string Model { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Description length only can be of 100 characters")]
+        public string Description { get; set; }
+
+        [Range(0, 9000, ErrorMessage = "Year only can be from 0 to 9,000")]
+        public int Year { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Brand length only can be of 50 characters")]
+        public string Brand { get; set; }
+
+        [Range(0, 1000000, ErrorMessage = "Kilometers only can be from 0 to 1,000,000")]
+        public int Kilometers { get; set; }
+
+        [Range(1, 10000000, ErrorMessage = "Price only can be from 1 to 10,000,000")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ HCApiTest/HCApiTest/Models/CarsContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCApiTest.Models
+{
+    public class CarsContext : DbContext
+    {
+        public CarsContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cars>().HasData(
+                new Cars() { Model = "Focus", Description = "sedan", Year = 1990, Brand = "Ford", Kilometers = 1500, Price = 120000 },
+                new Cars() { Model = "Focus", Description = "sedan", Year = 1990, Brand = "Ford", Kilometers = 1500, Price = 120000 },
+                new Cars() { Model = "Focus", Description = "sedan", Year = 1990, Brand = "Ford", Kilometers = 1500, Price = 120000 },
+                new Cars() { Model = "Focus", Description = "sedan", Year = 1990, Brand = "Ford", Kilometers = 1500, Price = 120000 },
+                new Cars() { Model = "Focus", Description = "sedan", Year = 1990, Brand = "Ford", Kilometers = 1500, Price = 120000 }                );
+        }
+        DbSet<Cars> Cars { get; set; }
+    }
+}
